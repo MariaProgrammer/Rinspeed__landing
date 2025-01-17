6f93fc764184bf82dffefdf47fd683689856c9d2
MariaProgrammer <maryfrontend22@gmail.com>
2025-01-18 00:08:10 +0300

    animation added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,156 @@
+// Плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substring(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
 // Burger
 const burger = document.getElementById('burger');
 const nav = document.getElementById('nav');
+const links = Array.from(document.querySelectorAll('burger__menu-link'));
 
 burger.addEventListener("click", () => {
-    nav.classList.toggle('active');
-    burger.classList.toggle('burger--active');
-    document.body.classList.toggle("stop-scroll");    
+  nav.classList.toggle('active');
+  burger.classList.toggle('burger--active');
+  // document.body.classList.toggle("stop-scroll");    
 });
+
+//cookies
+
+const cookiesBtn = document.querySelector('.cookie__button')
+const cookies = document.querySelector('.cookie')
+function getCookies() {
+  setTimeout(() => {
+    cookies.classList.add('active')
+  }, 1000)
+}
+cookiesBtn.addEventListener('click', function () {
+  cookies.classList.remove('active')
+})
+
+//Появление блоков по скроллу
+const hero = document.querySelector('.hero')
+const steps = document.querySelector('.steps')
+const recent = document.querySelector('.recent')
+const scrollCards = Array.from(document.querySelectorAll('.recent__card'))
+const scrollCards1 = [scrollCards[0], scrollCards[1], scrollCards[2]]
+const scrollCards2 = [scrollCards[3], scrollCards[4], scrollCards[5]]
+const scrollCards3 = [scrollCards[6], scrollCards[7], scrollCards[8]]
+
+const technique = document.querySelector('.technique')
+const contacts = document.querySelector('.contacts')
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(() => {
+    hero.classList.add('active')
+  }, 100)
+
+
+});
+
+document.addEventListener('scroll', function () {
+  let scrollTop = window.scrollY;
+
+  let heroHeight = hero.offsetHeight; 
+  let stepsHeight = hero.offsetHeight + steps.offsetHeight;
+  let recentCards = stepsHeight + recent.offsetHeight
+  let recentCards2 = stepsHeight + (recent.offsetHeight/3)
+  let recentCards5 = stepsHeight + (recent.offsetHeight/5)
+  let techniqueHeight = recentCards + technique.offsetHeight/3;
+  
+
+  if (scrollTop >= heroHeight / 3) {
+    steps.classList.add('active');
+  } else {
+    steps.classList.remove('active');
+  }
+
+  if (scrollTop >= stepsHeight) {
+    scrollCards1.forEach(card => {
+      card.classList.add('animation-class')
+    })
+  } else {
+    scrollCards1.forEach(card => {
+      card.classList.remove('animation-class')
+    })
+  }
+
+  if (scrollTop >= recentCards5) {
+    scrollCards2.forEach(card => {
+      card.classList.add('animation-class')
+    })
+  } else {
+    scrollCards2.forEach(card => {
+      card.classList.remove('animation-class')
+    })
+  }
+  if (scrollTop >= (recentCards5 + (recentCards5/5))) {
+    scrollCards3.forEach(card => {
+      card.classList.add('animation-class')
+    })
+  } else {
+    scrollCards3.forEach(card => {
+      card.classList.remove('animation-class')
+    })
+  }
+  if (scrollTop >= recentCards2) {
+    technique.classList.add('active');
+  } else {
+    technique.classList.remove('active');
+  }
+  if (scrollTop >= techniqueHeight) {
+    contacts.classList.add('active');
+  } else {
+    contacts.classList.remove('active');
+  }
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+let heroHeight = hero.offsetHeight;
+let stepsHeight = hero.offsetHeight + steps.offsetHeight;
+
+
+
+
+
+
+
+const scrollAnimation = () => {
+  let windowCenter = (window.innerHeight / 2) + window.scrollY;
+
+};
+
+scrollAnimation();
+window.addEventListener('scroll', () => {
+  scrollAnimation();
+});
+
+
+
+
 
 //Маска для тел
 
@@ -66,59 +210,59 @@ validation.addField("#name", [
 
 //Плавное исчезновение placeholder
 
-const nameField = document.getElementById('name') 
-const nameFieldCover = document.getElementById('placeholder1') 
+const nameField = document.getElementById('name')
+const nameFieldCover = document.getElementById('placeholder1')
 
-nameFieldCover.addEventListener('click', function(){
-	nameFieldCover.style.opacity = '0'
-	nameFieldCover.style.zIndex = '-1'
-	nameField.focus()
+nameFieldCover.addEventListener('click', function () {
+  nameFieldCover.style.opacity = '0'
+  nameFieldCover.style.zIndex = '-1'
+  nameField.focus()
 })
-nameField.addEventListener('focusout', function(e){
-	if (nameField.value == '') {
-		nameFieldCover.style.opacity = '1'
-		nameFieldCover.style.zIndex = '10'
-		nameField.blur()
-	}	
+nameField.addEventListener('focusout', function (e) {
+  if (nameField.value == '') {
+    nameFieldCover.style.opacity = '1'
+    nameFieldCover.style.zIndex = '10'
+    nameField.blur()
+  }
 })
 
 //изменение стиля карточки по наведению
 const cards = Array.from(document.querySelectorAll('.technique__card'))
 
 cards.forEach(card => {
-	card.addEventListener('mouseover', function(e){
-		let cardCurrent = e.currentTarget
-		
-		
-		cardCurrent.style.borderColor = '#DA7000'
-		let cardName = cardCurrent.querySelector('.technique__name')
-			
-		cardName.style.color = '#DA7000'
-	
-	})
+  card.addEventListener('mouseover', function (e) {
+    let cardCurrent = e.currentTarget
+
+
+    cardCurrent.style.borderColor = '#DA7000'
+    let cardName = cardCurrent.querySelector('.technique__name')
+
+    cardName.style.color = '#DA7000'
+
+  })
 
 })
 cards.forEach(card => {
-	card.addEventListener('mouseout', function(e){
-		let cardCurrent = e.currentTarget			
-		cardCurrent.style.borderColor = '#DDDDDD'
+  card.addEventListener('mouseout', function (e) {
+    let cardCurrent = e.currentTarget
+    cardCurrent.style.borderColor = '#DDDDDD'
 
-		let cardName = cardCurrent.querySelector('.technique__name')
-		cardName.style.color = '#0E0E0E'
-	})
+    let cardName = cardCurrent.querySelector('.technique__name')
+    cardName.style.color = '#0E0E0E'
+  })
 
 })
 cards.forEach(card => {
-	card.addEventListener('click', function(e){
-		let cardCurrent = e.currentTarget			
-		cardCurrent.style.borderColor = '#EE8414'
+  card.addEventListener('click', function (e) {
+    let cardCurrent = e.currentTarget
+    cardCurrent.style.borderColor = '#EE8414'
 
-		let cardName = cardCurrent.querySelector('.technique__name')
-		cardName.style.color = '#EE8414'
+    let cardName = cardCurrent.querySelector('.technique__name')
+    cardName.style.color = '#EE8414'
 
-		let cardImg = cardCurrent.querySelector('.technique__svg')
-		cardImg.classList.add('active')
-	})
+    let cardImg = cardCurrent.querySelector('.technique__svg')
+    cardImg.classList.add('active')
+  })
 })
 
 
